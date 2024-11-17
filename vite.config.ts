b4aef452673bdb59ca88d62322214dedb74e9a38
Ruslan Jafarov <rusyajaf@gmail.com>
2024-11-17 11:03:53 +0300

    chore: remove tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -23,7 +22,7 @@ export default defineConfig({
       }
     },
 		postcss: {
-			plugins: [tailwindcss(), autoprefixer()],
+			plugins: [autoprefixer()],
 		},
 	},
 });
