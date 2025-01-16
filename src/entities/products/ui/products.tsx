18221b7aa37a4934f5637fe8ffa708d7ba15ed0c
Ruslan Jafarov <rusyajaf@gmail.com>
2025-01-16 21:57:19 +0300

    feat: make link on image

--- src/entities/products/ui/products.tsx
+++ src/entities/products/ui/products.tsx
@@ -55,41 +55,41 @@ const ViewProduct: FC<IProduct> = ({ id, path, name, price }) => {
 	const [onItemHover, setOnItemHover] = useState<boolean>(false);
 
 	return (
-		<Link key={id} to={`/detail/${id}`}>
-			<FadeView className='mb-6'>
-				<div className='flex flex-col'>
-					<div
-						className='relative'
-						onMouseEnter={() => setOnItemHover(true)}
-						onMouseLeave={() => setOnItemHover(false)}
-					>
+		<FadeView className='mb-6'>
+			<div className='flex flex-col'>
+				<div
+					className='relative'
+					onMouseEnter={() => setOnItemHover(true)}
+					onMouseLeave={() => setOnItemHover(false)}
+				>
+					<Link key={id} to={`/detail/${id}`}>
 						<img
 							src={path}
 							alt={name}
 							className='w-full sm:w-[300px] md:w-[450px] h-auto mt-6 rounded-lg'
 							loading='lazy'
 						/>
-						{onItemHover && (
-							<motion.div
-								className='absolute top-8 right-2'
-								initial={{ opacity: 0 }}
-								animate={{ opacity: onItemHover ? 1 : 0 }}
-								transition={{ duration: 0.3 }}
-							>
-								<Favorite id={id} />
-							</motion.div>
-						)}
-					</div>
-
-					<div className='mt-4 mb-2.5'>
-						<Separator color={'bg-gray-200'} container={false} />
-					</div>
-					<div className='flex justify-between items-center'>
-						<h2>{name}</h2>
-						<h3 className='text-gray-400'>{price} ₽</h3>
-					</div>
+					</Link>
+					{onItemHover && (
+						<motion.div
+							className='absolute top-8 right-2'
+							initial={{ opacity: 0 }}
+							animate={{ opacity: onItemHover ? 1 : 0 }}
+							transition={{ duration: 0.3 }}
+						>
+							<Favorite id={id} />
+						</motion.div>
+					)}
 				</div>
-			</FadeView>
-		</Link>
+
+				<div className='mt-4 mb-2.5'>
+					<Separator color={'bg-gray-200'} container={false} />
+				</div>
+				<div className='flex justify-between items-center'>
+					<h2>{name}</h2>
+					<h3 className='text-gray-400'>{price} ₽</h3>
+				</div>
+			</div>
+		</FadeView>
 	);
 };
